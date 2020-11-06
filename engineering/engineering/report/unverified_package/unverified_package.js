118a5cacd945e65beb1c96c9116d47434df8c6f4
finbyz <info@finbyz.com>
2020-11-06 10:35:59 +0000

    CHanges Updated

--- engineering/engineering/report/unverified_package/unverified_package.js
+++ engineering/engineering/report/unverified_package/unverified_package.js
@@ -12,5 +12,35 @@ frappe.query_reports["Unverified Package"] = {
 			"reqd":1,
 			"default": frappe.defaults.get_user_default("Company")
 		},
+		{
+			"fieldname":"warehouse",
+			"label": __("Warehouse"),
+			"fieldtype": "Link",
+			"options": "Warehouse",
+			"get_query": function() {
+				const company = frappe.query_report.get_filter_value('company');
+				return { 
+					filters: { 'company': company }
+				}
+			}
+		},
+		{
+			"fieldname":"item_code",
+			"label": __("Item"),
+			"fieldtype": "Link",
+			"options": "Item",
+			"get_query": function() {
+				return {
+					query: "erpnext.controllers.queries.item_query"
+				}
+			}
+		},
+		{
+			"fieldname":"item_group",
+			"label": __("Item Group"),
+			"fieldtype": "Link",
+			"options": "Item Group"
+		},
+
 	]
 };
--- engineering/engineering/report/unverified_package/unverified_package.py
+++ engineering/engineering/report/unverified_package/unverified_package.py
@@ -16,7 +16,7 @@ def execute(filters=None):
 def get_data(filters):
 	query = frappe.db.sql("""
 	select
-		ip.name as package_no, ip.item_code, sno.warehouse, ip.no_of_items, ip.company, sno.status, ip.serial_no
+		ip.name as package_no, ip.item_code, ip.item_name,ip.item_group,sno.warehouse, ip.no_of_items, sno.company, sno.status, ip.serial_no
 	from
 		`tabItem Packing` as ip JOIN
 		`tabSerial No` as sno on sno.name = SUBSTRING_INDEX(ip.serial_no,'\n',1)
@@ -28,12 +28,20 @@ def get_data(filters):
 def get_conditions(filters):
 	condition = ""
 	if filters.get("company"):
-		condition = " and ip.company = '%s'" % filters.company
+		condition += " and sno.company = '%s'" % filters.company
+	if filters.get("item_code"):
+		condition += " and ip.item_code = '%s'" % filters.item_code
+	if filters.get("item_group"):
+		condition += " and ip.item_group = '%s'" % filters.item_group
+	if filters.get("warehouse"):
+		condition += " and sno.warehouse = '%s'" % filters.warehouse
 	return condition
 
 def get_columns(filters):
 	columns = [{"label": _("Package No"), "fieldname": "package_no", "fieldtype": "Link", "options": "Item Packing","width": 120},
-		{"label": _("Item Code"), "fieldname": "item_code", "options": "Item","width": 120},
+		{"label": _("Item Code"), "fieldname": "item_code", "fieldtype":"Link","options": "Item","width": 120},
+		{"label": _("Item Name"), "fieldname": "item_name", "fieldtype":"Data","width": 120},
+		{"label": _("Item Group"), "fieldname": "item_group", "fieldtype":"Link","options": "Item Group","width": 120},
 		{"label": _("Warehouse"), "fieldname": "warehouse", "fieldtype": "Link", "options": "Warehouse", "width": 200},
 		{"label": _("No of Pcs"), "fieldname": "no_of_items", "fieldtype": "Float","width": 80},
 		{"label": _("Company"), "fieldname": "company", "fieldtype": "Link","options": "Company","width": 220},
